0dfdf74baee7c02f322b8b097bf6d99d1a9aacbb
Meghamruthunjaya <57388067+Meghamruthunjaya@users.noreply.github.com>
2019-11-07 16:46:51 +0530

    updated Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello{
 
 	public static void main(String args[]){
 		for (int i=1;i<=10;i++){
+			System.out.orintln("My First Project 7/11");
 			System.out.println("Hello World....."+i);
 			System.out.println("Thanks for the webhooks demo..");
 			System.out.println("Thanks for the webhooks demo1..");
